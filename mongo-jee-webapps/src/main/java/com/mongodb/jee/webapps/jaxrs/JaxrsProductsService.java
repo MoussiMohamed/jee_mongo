b84889db1929ecc70915d3a98fb9da08c9712159
MoussiMohamed <msi.moussi@gmail.com>
2014-03-26 11:03:05 +0100

    configuration de jsf dans JAXRS

--- mongo-jee-webapps/src/main/java/com/mongodb/jee/webapps/jaxrs/JaxrsProductsService.java
+++ mongo-jee-webapps/src/main/java/com/mongodb/jee/webapps/jaxrs/JaxrsProductsService.java
@@ -1,6 +1,5 @@
 package com.mongodb.jee.webapps.jaxrs;
 
-import com.mongodb.BasicDBList;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -14,14 +13,17 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.model.UploadedFile;
 import dojo.store.PageRangeRequest;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.UnknownHostException;
 import java.util.Iterator;
 import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -31,7 +33,7 @@ import org.codehaus.jettison.json.JSONObject;
 
 @Path("/")
 public class JaxrsProductsService {
-
+    
 	@GET
 	@Path("/init")
 	public void initializeData() {
@@ -45,94 +47,25 @@ public class JaxrsProductsService {
 			products.insert(product);
 		}
 	}
-
+        
 	@GET
 	@Path("/products")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public String findProducts(@HeaderParam("Range") PageRangeRequest range) throws UnknownHostException {
-		
  
         return read();
-//        return coll.find().toString();
-                
-//		return new PageResult(products.find(), range.getFromIndex(),
-//				range.getToIndex());
 	}
-//        DBObject cells=null;
-//        private DBObject read(){
-//            try
-//        {
-//            FileInputStream file = new FileInputStream(new File("/home/msi/Bureau/inv-04-02-2014.xls"));
-// 
-//            //Create Workbook instance holding reference to .xlsx file
-//            XSSFWorkbook workbook = new XSSFWorkbook(file);
-//            //Get first/desired sheet from the workbook
-//            XSSFSheet sheet = workbook.getSheetAt(0);
-//            JSONArray rows = new JSONArray();
-//            //Iterate through each rows one by one
-//            Iterator<Row> rowIterator = sheet.iterator();
-//            while (rowIterator.hasNext()) 
-//            {
-//                Row row = rowIterator.next();
-//                JSONObject jRow = new JSONObject();
-//                cells = new BasicDBObject();
-//                //For each row, iterate through all the columns
-//                Iterator<Cell> cellIterator = row.cellIterator();
-//                 
-//                while (cellIterator.hasNext()) 
-//                {
-//                    Cell cell = cellIterator.next();
-//                    //Check the cell type and format accordingly
-//                    switch (cell.getCellType()) 
-//                    {
-//                        case Cell.CELL_TYPE_NUMERIC:
-//                            cells.put("Quantite", cell.getNumericCellValue() );
-//                            break;
-//                        case Cell.CELL_TYPE_STRING:
-//                            if (cell.getColumnIndex()==0){
-//                            cells.put("Reference",cell.getStringCellValue());
-//                            }else if(cell.getColumnIndex()==1)
-//                                cells.put("Designation",cell.getStringCellValue() );
-//                            else if(cell.getColumnIndex()==3)
-//                                cells.put("Depot",cell.getStringCellValue() );
-//                            break;
-//                                
-//                    }
-//                }        
-//            }
-////             MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-//            
-//            DB db = MongoHolder.connect().getDB("mydb");
-//        DBCollection coll = db.getCollection("Collection1");
-//        System.out.println("Count: " + coll.count());
-//        
-//        coll.insert(cells);
-//            System.out.println(cells);
-//            file.close();
-//        } 
-//        catch (Exception e) 
-//        {
-//            e.printStackTrace();
-//        }
-//            return cells;
-//        }
-        JSONObject json;
-        JSONArray cells;
+        
         JSONArray rows;
         private String read(){
             try
         {
             FileInputStream file = new FileInputStream(new File("/home/msi/Bureau/inv-04-02-2014.xls"));
- 
             //Create Workbook instance holding reference to .xlsx file
             XSSFWorkbook workbook = new XSSFWorkbook(file);
- 
             //Get first/desired sheet from the workbook
             XSSFSheet sheet = workbook.getSheetAt(0);
- 
-            // Start constructing JSON.
-            json = new JSONObject();
             DB db = MongoHolder.connect().getDB("mydb");
             DBCollection coll = db.getCollection("Collection1");
             // Iterate through the rows.
@@ -143,10 +76,8 @@ public class JaxrsProductsService {
             {
                 Row row = rowIterator.next();
                 JSONObject jRow = new JSONObject();
-                cells = new JSONArray();
                 //For each row, iterate through all the columns
                 Iterator<Cell> cellIterator = row.cellIterator();
-                 
                 while (cellIterator.hasNext())
                 {
                     Cell cell = cellIterator.next();
@@ -154,14 +85,7 @@ public class JaxrsProductsService {
                     //Check the cell type and format accordingly
                     switch (cell.getCellType()) 
                     {
-//                        case Cell.CELL_TYPE_NUMERIC:
-//
-//                            cells.put( cell.getNumericCellValue() );
-//                            break;
-//                        case Cell.CELL_TYPE_STRING:
-//
-//                            cells.put( cell.getStringCellValue() );
-//                            break;
+
                         case Cell.CELL_TYPE_NUMERIC:
                             jRow.put("Quantite", cell.getNumericCellValue() );
                             break;
@@ -174,14 +98,9 @@ public class JaxrsProductsService {
                                 jRow.put("Depot",cell.getStringCellValue() );
                             break;
                     }
-//                    jRow.put( "cell", cells );
                     rows.put(jRow);
-                }
-                
-//                json.put( "rows", rows );
-                
+                }   
             }
-//            for()
             System.out.println(rows);
             coll.insert((List<DBObject>) rows);
             file.close();
@@ -192,4 +111,24 @@ public class JaxrsProductsService {
         }
             return rows.toString();
         }
+        @ManagedBean
+        @SessionScoped
+        public class fileUploadController {
+    private UploadedFile file;
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public void upload() {
+        if(file != null) {
+            FacesMessage msg = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+}
 }
